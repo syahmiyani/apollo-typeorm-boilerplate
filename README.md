659c136a224a933c0b39b89c6e45bdf1609a6327
Syahmi Yani <syahmiyani@gmail.com>
2020-01-05 13:55:07 +0800

    Typescript GraphQL CRUD

---
+++ README.md
@@ -0,0 +1,7 @@
+#  A GraphQL server with Typescript for CRUD operations using Apollo, TypeORM, and Type-GraphQL.
+
+## An example of how a modern GraphQL server with TypeORM should look like.
+
+[YouTube video associated with this repo](https://www.youtube.com/watch?v=WhzIjYQmWvs)
+
+
---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import "reflect-metadata";
+import { createConnection } from "typeorm";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+
+// Cumulative Resolvers
+import { HelloWorldResolver } from "./resolvers/HelloWorldResolver";
+
+(async () => {
+  const app = express();
+
+  await createConnection();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloWorldResolver]
+    }),
+    context: ({ req, res }) => ({ req, res })
+  });
+
+  apolloServer.applyMiddleware({ app, cors: false });
+
+  app.listen(4000, () => {
+    console.log("server started at :4000");
+  });
+})();
